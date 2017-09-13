319525df8ce8b1f72437333136c948b06f306399
2hill <jamil.rahjou@laposte.net>
2017-09-14 01:45:46 +0200

    job almost done:checkered_flag:

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 "use strict";
 
+let check = document.querySelector(".form")
 let error = document.querySelector(".error")
-let succes = document.querySelector(".success")
-let check = document.querySelector(".shopify")
+let success = document.querySelector(".success")
+let email = document.getElementById("email").value
 let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/
 
 check.addEventListener('submit', function(e) {
-  (!regex.test(check))
-        ?(error.style.display = "inline")
-        :(success.style.display = "inline")
-        e.preventDefault()
-  }
-);
+  (!regex.test(email))
+    ? (error.style.display = "inline")
+    : (success.style.display = "inline")
+    e.preventDefault()
+});
