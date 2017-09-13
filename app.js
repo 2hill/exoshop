5dd10793ffd71f7b829985a0bd2a0dce7cd83f74
2hill <jamil.rahjou@laposte.net>
2017-09-13 23:11:57 +0200

    add submit event

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 
+let check = document.querySelector('.shopify')
+check.addEventListener('submit', function(e) {
+
+  console.log(e);
+
+  /*let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+  let error = document.getElementsByClassName('.error');
+
+  if (!regex.test(e.value)) {
+    error.style.display = "inline";
+
+  } else {
+    alert(message : "Welcome to our newsletter")
+  }
+}*/
+});
