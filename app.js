84962e242466fa4761c14c842438d8b32b214dac
2hill <jamil.rahou@laposte.net>
2018-05-02 15:30:44 +0200

    testing lada

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 // Declaring my variables in preparation of input checking
 
 let check = document.querySelector("form")
@@ -10,10 +9,8 @@ let success = document.querySelector(".success")
 let email = document.querySelector("#email")
 let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/
 
-
-
 /* Check regex, show success/error message, disable click after success. 
-(In general I use an if statement but here I wanted to experiment/try the ternary operators) */
+(In general I use an if statement but here I wanted to experiment the ternary operator) */
 
 check.addEventListener('submit', function (e) {
   e.preventDefault()
@@ -22,20 +19,8 @@ check.addEventListener('submit', function (e) {
     :(success.style.display = "inline", console.log("good input"), error.remove(), button.disabled = true)
 });
 
-/*if (!regex.test(email.value)) {
-  return (error.style.display = "inline", console.log("wrong input"))
-}
-return (success.style.display = "inline", console.log("good input"), error.remove());
-*/
-
-// disable button after one click
-
-
-
 // Simulating HTTP request using Ladda built-in loading indicators
-Ladda.bind('button[type=submit]', {
-  timeout: 2000
-});
+Ladda.bind('button[type=submit]');
 
 
 
