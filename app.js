c224d18fa247dbb7bcf0bc550bd65770887e2c1b
2hill <jamil.rahou@laposte.net>
2018-05-05 12:55:18 +0200

    log input

--- app.js
+++ app.js
@@ -7,21 +7,30 @@ let button = document.querySelector(".button")
 let error = document.querySelector(".error")
 let success = document.querySelector(".success")
 let email = document.querySelector("#email")
+let select = document.querySelector("#select")
 let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/
 
-/* Check regex, show success/error message, disable click after success. 
+/* Check regex, show success/error message, disable click only after success. 
 (In general I use an if statement but here I wanted to experiment the ternary operator) */
-
 check.addEventListener('submit', function (e) {
   e.preventDefault()
     !regex.test(email.value)
-    ?(error.style.display = "inline", console.log("wrong input"))
-    :(success.style.display = "inline", console.log("good input"), error.remove(), button.disabled = true)
+    ?(error.style.display = "inline",
+      console.log(
+          "wrong format:", 
+          email.value, 
+          select.options[select.selectedIndex].text)) 
+    :(success.style.display = "inline",
+      console.log(
+          "valid format:",
+          email.value,
+          select.options[select.selectedIndex].text),
+          error.remove(), 
+          button.disabled = true)
 });
 
 // Simulating HTTP request using Ladda built-in loading indicators
 Ladda.bind('button[type=submit]');
-
 
 
 //  Jquery function to simulate loading state
